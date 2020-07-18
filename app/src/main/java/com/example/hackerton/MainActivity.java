d7706c5bff6eae3b2c157fcd328c776a9b05ddc5
CHP <versatile1226@naver.com>
2020-07-18 16:41:15 +0900

    최화평수정

--- app/src/main/java/com/example/hackerton/MainActivity.java
+++ app/src/main/java/com/example/hackerton/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //수정
+    //최화평 수정ddd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
