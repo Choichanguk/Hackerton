419aa93b31276ff9cac2438a8be70afa592d4e19
KDB <pok_ehdqls@naver.com>
2020-07-18 16:44:08 +0900

    테스트김동빈입니다.

--- app/src/main/java/com/example/hackerton/MainActivity.java
+++ app/src/main/java/com/example/hackerton/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //수정
     //최화평 수정ddd
+    //김동빈
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
