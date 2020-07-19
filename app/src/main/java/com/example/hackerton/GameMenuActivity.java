e3dc4ec6d11ad285417193e1aebf7826b5c9a5b2
KUM <mkw04e@gmail.com>
2020-07-20 03:52:11 +0900

    메인 페이지 변경 매니페스트 수정 함- 건욱

--- app/src/main/java/com/example/hackerton/GameMenuActivity.java
+++ app/src/main/java/com/example/hackerton/GameMenuActivity.java
@@ -2,12 +2,14 @@ package com.example.hackerton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class GameMenuActivity extends AppCompatActivity {
     Button btnStartAvoidGame; //장애물피하기 게임버튼
+    Button btnStartWeightliftingGame; // 역도 게임 버튼
     Button btnStartBombGame;  //폭탄돌리기 게임버튼
 
     @Override
@@ -16,13 +18,22 @@ public class GameMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_menu);
 
         btnStartAvoidGame = findViewById(R.id.btnStartAvoidGame);
+        btnStartWeightliftingGame = findViewById(R.id.btnStartWeightliftingGame);
         btnStartBombGame = findViewById(R.id.btnStartBombGame);
 
         btnStartAvoidGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStartWeightliftingGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Weightlifting.class);
+                startActivity(intent);
             }
         });
 
